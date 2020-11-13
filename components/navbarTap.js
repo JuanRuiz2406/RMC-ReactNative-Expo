8d04324d76f8914fd33b283cd714850b3600c985
dvperga <44186637+dvperga@users.noreply.github.com>
2020-11-12 23:11:25 -0600

    nav bar completo

--- components/navbarTap.js
+++ components/navbarTap.js
@@ -5,6 +5,9 @@ import { AntDesign } from '@expo/vector-icons';
 
 import Home from './home'
 import Reports from './reports'
+import Report from './report'
+import CreateReport from './createReport'
+import Profile from './profile'
 
 const Tab = createBottomTabNavigator();
 
@@ -21,7 +24,26 @@ export default function MyTabs() {
                 options={{
                     tabBarLabel: 'Home',
                     tabBarIcon: ({ color }) => (
-                        <AntDesign name="home" color={color} size={26} />
+                        <AntDesign name="home" color={color} size={30} />
+                    ),
+                }}
+            />
+            <Tab.Screen
+                name="Buscar"
+                component={Report}
+                options={{
+                    tabBarIcon: ({ color }) => (
+                        <AntDesign name="search1" color={color} size={30} />
+                    ),
+                }}
+            />
+            <Tab.Screen
+                name="Crear Reporte"
+                component={CreateReport}
+                options={{
+                    tabBarLabel: 'Crear Reporte',
+                    tabBarIcon: ({ color }) => (
+                        <AntDesign name="plus" color={color} size={40} />
                     ),
                 }}
             />
@@ -31,10 +53,21 @@ export default function MyTabs() {
                 options={{
                     tabBarLabel: 'Reports',
                     tabBarIcon: ({ color }) => (
-                        <AntDesign name="notification" color={color} size={26} />
+                        <AntDesign name="bells" color={color} size={30} />
                     ),
                 }}
             />
+            <Tab.Screen
+                name="Perfil"
+                component={Profile}
+                options={{
+                    tabBarLabel: 'Perfil',
+                    tabBarIcon: ({ color }) => (
+                        <AntDesign name="user" color={color} size={30} />
+                    ),
+                }}
+            />
+
         </Tab.Navigator>
     );
 }
