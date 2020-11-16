85d3ec3fa02b4357f8075c50c59c41dc5c81b091
Juan <juanruizh2406@gmail.com>
2020-11-16 17:53:21 -0600

    Vista Crear Reporte sin Imgs
    PARA PRUEBAS CAMBIAR DIRECCION IP

--- components/createReport.js
+++ components/createReport.js
@@ -1,101 +1,156 @@
 import React, { Component } from "react";
-import { View, Text, Button, TextInput, Alert } from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  Alert,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
+
+const styles = StyleSheet.create({
+  Title: {
+    textAlign: "center",
+    fontSize: 25,
+    margin: 20,
+    fontWeight: "bold",
+  },
+  InputText: {
+    padding: 4,
+    borderWidth: 1,
+    borderRadius: 10,
+    borderColor: "black",
+    marginTop: 20,
+    marginLeft: 30,
+    marginRight: 30,
+    marginBottom: 30,
+    fontSize: 20,
+  },
+  Text: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginTop: 10,
+    marginLeft: 40,
+  },
+  Btn: {
+    color: "#F7751E",
+    marginTop: 50,
+  },
+  button: {
+    backgroundColor: "#FEB139",
+    padding: 7,
+    marginLeft: 120,
+    marginRight: 120,
+    borderWidth: 1,
+    borderRadius: 10,
+    borderColor: "black",
+  },
+  buttonText: {
+    fontSize: 20,
+    textAlign: "center",
+    color: "#fff",
+    fontWeight: "bold",
+},
+});
 
 export default class App extends React.Component {
-    createTwoButtonAlert = (msg) =>
-        Alert.alert(
-            "Reporte",
-            msg,
-            [
-                { text: "OK", onPress: () => console.log("OK Pressed") }
-            ],
-            { cancelable: false }
+  createTwoButtonAlert = (msg) =>
+    Alert.alert(
+      "Reporte",
+      msg,
+      [{ text: "OK", onPress: () => console.log("OK Pressed") }],
+      { cancelable: false }
+    );
+
+  constructor() {
+    super();
+    this.state = {
+      description: "",
+      state: "",
+      privacy: "",
+      user: "",
+    };
+  }
+
+  submit() {
+    fetch("http://192.168.0.3:8080/reports", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        description: this.state.description,
+        state: this.state.state,
+        privacy: this.state.privacy,
+        user: this.state.user,
+      }),
+    })
+      .then((response) => response.json())
+      .then((responseJson) => {
+        this.setState(
+          {
+            isLoading: false,
+            dataSource: responseJson,
+          },
+          this.createTwoButtonAlert(responseJson.messageString)
         );
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
 
-    constructor() {
-        super();
-        this.state = {
-            description: "",
-            state: "",
-            privacy: "",
-            user: "",
-        };
-    }
+  render() {
+    return (
+      <View>
+        <Text style={styles.Title}>Crear Reporte</Text>
 
-    submit() {
-        fetch("http://192.168.1.112:8080/reports", {
-            method: "POST",
-            headers: {
-                Accept: "application/json",
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                description: this.state.description,
-                state: this.state.state,
-                privacy: this.state.privacy,
-                user: this.state.user,
-            }),
-        }).then((response) => response.json())
-            .then((responseJson) => {
-                this.setState(
-                    {
-                        isLoading: false,
-                        dataSource: responseJson,
-                    },
-                    this.createTwoButtonAlert(responseJson.messageString)
-                );
-            })
-            .catch((error) => {
-                console.error(error);
-            });
-    }
+        
+        <Text style={styles.Text}>Descripción</Text>
+        <TextInput
+          placeholder="   Ingresa la descripción del Reporte"
+          onChangeText={(text) => {
+            this.setState({ description: text });
+          }}
+          style={styles.InputText}
+        />
 
-    render() {
-        return (
-            <View style={{ margin: 20, marginTop: 100 }}>
-                <Text>Descripción</Text>
-                <TextInput
-                    placeholder="   Ingresa la descripción del Reporte"
-                    onChangeText={(text) => {
-                        this.setState({ description: text });
-                    }}
-                    style={{ borderWidth: 2, borderColor: "black", margin: 30 }}
-                />
+        <Text style={styles.Text}>Estado</Text>
+        <TextInput
+          placeholder="   Ingresa el Estado"
+          onChangeText={(text) => {
+            this.setState({ state: text });
+          }}
+          style={styles.InputText}
+        />
 
-                <Text>Estado</Text>
-                <TextInput
-                    placeholder="   Ingresa el Estado"
-                    onChangeText={(text) => {
-                        this.setState({ state: text });
-                    }}
-                    style={{ borderWidth: 2, borderColor: "black", margin: 30 }}
-                />
+        <Text style={styles.Text}>Privacidad</Text>
+        <TextInput
+          placeholder="   Ingresa la privacidad del Reporte"
+          onChangeText={(text) => {
+            this.setState({ privacy: text });
+          }}
+          style={styles.InputText}
+        />
 
-                <Text>Privacidad</Text>
-                <TextInput
-                    placeholder="   Ingresa la privacidad del Reporte"
-                    onChangeText={(text) => {
-                        this.setState({ privacy: text });
-                    }}
-                    style={{ borderWidth: 2, borderColor: "black", margin: 30 }}
-                />
+        <Text style={styles.Text}>Id de Usuario</Text>
+        <TextInput
+          placeholder="   Ingresa el ID de tu Usuario"
+          onChangeText={(text) => {
+            this.setState({ user: text });
+          }}
+          style={styles.InputText}
+        />
 
-                <Text>Id de Usuario</Text>
-                <TextInput
-                    placeholder="   Ingresa el ID de tu Usuario"
-                    onChangeText={(text) => {
-                        this.setState({ user: text });
-                    }}
-                    style={{ borderWidth: 2, borderColor: "black", margin: 30 }}
-                />
+        <TouchableOpacity
+          onPress={() => this.submit()}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>Reportar</Text>
+        </TouchableOpacity>
 
-                <Button
-                    title="Reportar"
-                    onPress={() => {
-                        this.submit();
-                    }}
-                />
-            </View>
-        );
-    }
+      </View>
+    );
+  }
 }
