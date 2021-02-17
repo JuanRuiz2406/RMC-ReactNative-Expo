c9da0435a21f8efa2271bea8d4998c30f3ea8bd3
JuanRuiz2406 <44301111+JuanRuiz2406@users.noreply.github.com>
2021-02-17 00:04:31 -0600

    formatear código - index.js
    DonutChart tiene codigo comentado que no se utiliza

--- components/loginScreen.js
+++ components/loginScreen.js
@@ -1,96 +1,90 @@
-import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
-import { Heading } from './loginComponents/heading';
-import { Input } from './loginComponents/input';
-import { FilledButton } from './loginComponents/filledButton';
-import { TextButton } from './loginComponents/textButton';
-//import { Error } from './loginComponents/error';
-import { AuthContainer } from './loginComponents/authContainer';
-import { AuthContext } from './contexts/authContext';
+import React, { useContext, useState } from "react";
+import { StyleSheet } from "react-native";
+import {
+  Heading,
+  Input,
+  FilledButton,
+  TextButton,
+  AuthContainer,
+  AuthContext,
+  //Error,
+} from "./index";
 
 export function LoginScreen({ navigation }) {
+  const { login } = useContext(AuthContext);
+  const [data, setData] = useState({
+    username: "",
+    password: "",
+    check_textInputChange: true,
+    secureTextEntry: true,
+  });
 
-    const { login } = React.useContext(AuthContext);
-    const [data, setData] = React.useState({
-        username: '',
-        password: '',
-        check_textInputChange: true,
-        secureTextEntry: true
+  const inputChange = (val) => {
+    setData({
+      ...data,
+      username: val,
     });
+  };
 
-    const inputChange = (val) => {
-        setData({
-            ...data,
-            username: val,
-        });
-    }
+  const handlePasswordChange = (val) => {
+    setData({
+      ...data,
+      password: val,
+    });
+  };
 
-    const handlePasswordChange = (val) => {
-        setData({
-            ...data,
-            password: val,
-        });
-    }
+  const loginHandle = (username, password) => {
+    login(username, password);
+  };
 
-    const loginHandle = (username, password) => {
-        login(username, password);
-    }
+  return (
+    <AuthContainer>
+      <Heading style={styles.title}>ReportMyCity</Heading>
+      <Heading style={styles.title}>LOGIN</Heading>
 
-    return (
-        <AuthContainer>
-            <Heading style={styles.title}>
-                ReportMyCity
-            </Heading>
-            <Heading style={styles.title}>
-                LOGIN
-            </Heading>
+      <Input
+        style={styles.input}
+        placeholder={"Nombre de Usuario"}
+        keyboardType={"email-address"}
+        onChangeText={(val) => inputChange(val)}
+      />
 
-            <Input style={styles.input}
-                placeholder={'Nombre de Usuario'}
-                keyboardType={'email-address'}
-                onChangeText={(val) => inputChange(val)}
-            />
+      <Input
+        style={styles.input}
+        placeholder={"Contraseña"}
+        secureTextEntry={true}
+        onChangeText={(val) => handlePasswordChange(val)}
+      />
 
-            <Input style={styles.input}
-                placeholder={'Contraseña'}
-                secureTextEntry={true}
-                onChangeText={(val) => handlePasswordChange(val)}
-            />
+      <FilledButton
+        title={"Login"}
+        style={styles.loginButton}
+        onPress={() => {
+          loginHandle(data.username, data.password);
+        }}
+      />
 
-            <FilledButton
-                title={'Login'}
-                style={styles.loginButton}
-                onPress={() => {
-                    loginHandle(data.username, data.password)
-                }}
-            />
-
-            <TextButton
-                title={'Tienes una cuanta? Crear una.'}
-                onPress={() => {
-                    navigation.navigate('RegisterScreen');
-                }}
-            />
-        </AuthContainer>
-    );
+      <TextButton
+        title={"No tienes una cuenta? Crea una"}
+        onPress={() => {
+          navigation.navigate("RegisterScreen");
+        }}
+      />
+    </AuthContainer>
+  );
 }
 
-
-
 const styles = StyleSheet.create({
-    container: {
-        alignItems: 'center',
-    },
-
-    input: {
-        marginVertical: 8,
-    },
-
-    title: {
-        marginBottom: 48,
-    },
-
-    loginButton: {
-        marginVertical: 32,
-    },
+  container: {
+    alignItems: "center",
+  },
+  input: {
+    marginVertical: 8,
+  },
+  title: {
+    marginBottom: 48,
+  },
+  loginButton: {
+    marginVertical: 32,
+  },
 });
--- components/registerScreen.js
+++ components/registerScreen.js
@@ -1,95 +1,95 @@
-import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
-import { Heading } from './loginComponents/heading';
-import { Input } from './loginComponents/input';
-import { FilledButton } from './loginComponents/filledButton';
-import { IconButton } from './loginComponents/iconButton'
-import { AuthContainer } from './loginComponents/authContainer';
-import { AuthContext } from './contexts/authContext';
-
+import React, { useContext, useState } from "react";
+import { StyleSheet } from "react-native";
+import {
+  Heading,
+  Input,
+  FilledButton,
+  IconButton,
+  AuthContainer,
+  AuthContext,
+} from "./index";
 
 export function RegisterScreen({ navigation }) {
+  const { register } = useContext(AuthContext);
+  const [email, setEmail] = useState(() => "diegovillatj@gmail.com");
+  const [name, setName] = useState(() => "Diego");
+  const [lasname, setLastname] = useState(() => "Villarreal");
+  const [idCard, setId] = useState(() => "504200201");
+  const [direction, setDirection] = useState(() => "Liberia");
+  const [role, setRole] = useState(() => "user");
+  const [password, setPassword] = useState(() => "abc");
+  const [loading, setLoading] = useState(() => false);
+  const [error, setError] = useState(() => "");
 
-    const { register } = React.useContext(AuthContext);
-    const [email, setEmail] = React.useState(() => 'diegovillatj@gmail.com');
-    const [name, setName] = React.useState(() => 'Diego');
-    const [lasname, setLastname] = React.useState(() => 'Villarreal');
-    const [idCard, setId] = React.useState(() => '504200201');
-    const [direction, setDirection] = React.useState(() => 'Liberia');
-    const [role, setRole] = React.useState(() => 'user');
-    const [password, setPassword] = React.useState(() => 'abc');
-    const [loading, setLoading] = React.useState(() => false);
-    const [error, setError] = React.useState(() => '');
+  return (
+    <AuthContainer>
+      <IconButton
+        style={styles.closeIcon}
+        name={"close-circle-outline"}
+        onPress={() => {
+          navigation.pop();
+        }}
+      />
 
-    return (
-        <AuthContainer>
+      <Heading style={styles.title}>REGISTRO</Heading>
 
-            <IconButton style={styles.closeIcon} name={'close-circle-outline'} onPress={() => {
-                navigation.pop();
-            }} />
+      <Input
+        style={styles.input}
+        placeholder={"Nombre"}
+        value={name}
+        onChangeText={setName}
+      />
+      <Input
+        style={styles.input}
+        placeholder={"Apellido"}
+        value={lasname}
+        onChangeText={setLastname}
+      />
+      <Input
+        style={styles.input}
+        placeholder={"Numero de identificacion"}
+        value={idCard}
+        onChangeText={setId}
+      />
+      <Input
+        style={styles.input}
+        placeholder={"Nombre de Usuario"}
+        keyboardType={"email-address"}
+        value={email}
+        onChangeText={setEmail}
+      />
+      <Input
+        style={styles.input}
+        placeholder={"Contraseña"}
+        secureTextEntry
+        value={password}
+        onChangeText={setPassword}
+      />
 
-            <Heading style={styles.title}>
-                REGISTRO
-            </Heading>
-
-            <Input style={styles.input}
-                placeholder={'Nombre'}
-                value={name}
-                onChangeText={setName}
-            />
-            <Input style={styles.input}
-                placeholder={'Apellido'}
-                value={lasname}
-                onChangeText={setLastname}
-            />
-            <Input style={styles.input}
-                placeholder={'Numero de identificacion'}
-                value={idCard}
-                onChangeText={setId}
-            />
-            <Input style={styles.input}
-                placeholder={'Nombre de Usuario'}
-                keyboardType={'email-address'}
-                value={email}
-                onChangeText={setEmail}
-            />
-            <Input style={styles.input}
-                placeholder={'Contraseña'}
-                secureTextEntry
-                value={password}
-                onChangeText={setPassword}
-            />
-
-            <FilledButton
-                title={'Registro'}
-                style={styles.RegisterButton}
-                onPress={() => {
-                    register();
-                }}
-            />
-        </AuthContainer>
-    );
+      <FilledButton
+        title={"Registro"}
+        style={styles.RegisterButton}
+        onPress={() => {
+          register();
+        }}
+      />
+    </AuthContainer>
+  );
 }
 
-
 const styles = StyleSheet.create({
-
-    input: {
-        marginVertical: 8,
-    },
-
-    title: {
-        marginBottom: 48,
-    },
-
-    RegisterButton: {
-        marginVertical: 32,
-    },
-
-    closeIcon: {
-        position: 'absolute',
-        top: 60,
-        right: 16,
-
-    },
+  input: {
+    marginVertical: 8,
+  },
+  title: {
+    marginBottom: 48,
+  },
+  RegisterButton: {
+    marginVertical: 32,
+  },
+  closeIcon: {
+    position: "absolute",
+    top: 60,
+    right: 16,
+  },
 });
