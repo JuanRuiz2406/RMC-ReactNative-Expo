7684cb38c22a10e8916bc9c7c9236a0e01f341d5
JuanRuiz2406 <44301111+JuanRuiz2406@users.noreply.github.com>
2021-02-17 00:55:23 -0600

    Ultimo formateo
    se comentan rutas no usadas de index

--- components/home.js
+++ components/home.js
@@ -1,32 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, Button, ScrollView } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { View, ScrollView } from "react-native";
 
-import Carousel from './carousel'
-import { dummyData } from '../data/data'
-import Donut from './donutChart'
-import { circleData } from '../data/circleData'
+import { Carousel, Donut } from "./index";
+import { dummyData } from "../data/data";
+import { circleData } from "../data/circleData";
 
 const Home = () => {
-    return (
-        <ScrollView showsVerticalScrollIndicator={false}>
-            <Carousel data={dummyData} />
-            <StatusBar style="auto" />
-            <View
-                style={{
-                    backgroundColor: 'transparent',
-                    alignItems: 'center',
-                    justifyContent: 'space-evenly',
-                    flexWrap: 'wrap',
-                    flexDirection: 'row'
-                }}>
-                {circleData.map((p, i) => {
-                    return <Donut key={i} percentage={p.percentage} color={p.color} delay={1000} max={p.max} radius={p.radius} texto={p.texto} />
-                })}
-            </View>
-
-        </ScrollView>
-    );
+  return (
+    <ScrollView showsVerticalScrollIndicator={false}>
+      <Carousel data={dummyData} />
+      <StatusBar style="auto" />
+      <View
+        style={{
+          backgroundColor: "transparent",
+          alignItems: "center",
+          justifyContent: "space-evenly",
+          flexWrap: "wrap",
+          flexDirection: "row",
+        }}
+      >
+        {circleData.map((p, i) => {
+          return (
+            <Donut
+              key={i}
+              percentage={p.percentage}
+              color={p.color}
+              delay={1000}
+              max={p.max}
+              radius={p.radius}
+              texto={p.texto}
+            />
+          );
+        })}
+      </View>
+    </ScrollView>
+  );
 };
 
 export default Home;
