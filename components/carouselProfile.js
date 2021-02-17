37b2a4dbc11fe3fe15b7661e06293525358452e9
JuanRuiz2406 <44301111+JuanRuiz2406@users.noreply.github.com>
2021-02-17 00:47:23 -0600

    más formato

--- components/carouselProfile.js
+++ components/carouselProfile.js
@@ -1,48 +1,50 @@
-import React, { useState, useEffect } from 'react'
-import { View, Text, StyleSheet, Dimensions, FlatList, Animated } from 'react-native'
-import CarouselItem from './carouselItemProfile'
+import React from "react";
+import {
+  View,
+  StyleSheet,
+  Dimensions,
+  FlatList,
+  Animated,
+} from "react-native";
+import CarouselItem from "./carouselItemProfile";
 
-const { width, height } = Dimensions.get('window')
+const { width, height } = Dimensions.get("window");
 
-const Carousel = ({ data }) => {
-    const scrollX = new Animated.Value(0)
-    let position = Animated.divide(scrollX, width)
+export default Carousel = ({ data }) => {
+  const scrollX = new Animated.Value(0);
+  let position = Animated.divide(scrollX, width);
 
+  if (data && data.length) {
+    return (
+      <View>
+        <FlatList
+          data={data}
+          keyExtractor={(item, index) => "key" + index}
+          horizontal
+          pagingEnabled
+          scrollEnabled
+          snapToAlignment="center"
+          scrollEventThrottle={16}
+          decelerationRate={"fast"}
+          showsHorizontalScrollIndicator={false}
+          renderItem={({ item }) => {
+            return <CarouselItem item={item} />;
+          }}
+          onScroll={Animated.event([
+            { nativeEvent: { contentOffset: { x: scrollX } } },
+          ])}
+        />
+      </View>
+    );
+  }
 
-    if (data && data.length) {
-        return (
-            <View>
-                <FlatList
-                    data={data}
-                    keyExtractor={(item, index) => 'key' + index}
-                    horizontal
-                    pagingEnabled
-                    scrollEnabled
-                    snapToAlignment='center'
-                    scrollEventThrottle={16}
-                    decelerationRate={"fast"}
-                    showsHorizontalScrollIndicator={false}
-                    renderItem={({ item }) => {
-                        return <CarouselItem item={item} />
-                    }}
-                    onScroll={Animated.event(
-                        [{ nativeEvent: { contentOffset: { x: scrollX } } }]
-                    )}
-                />
-
-            </View>
-        )
-    }
-
-    console.log('Please provide images')
-    return null
-}
+  console.log("Please provide images");
+  return null;
+};
 
 const styles = StyleSheet.create({
-    dotView: {
-        flexDirection: 'row',
-        justifyContent: 'center'
-    },
-})
-
-export default Carousel
+  dotView: {
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+});
