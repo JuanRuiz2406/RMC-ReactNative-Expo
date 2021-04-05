ef2ecd6c84132e122a790a6f62d8c1369332f00c
JuanRuiz2406 <44301111+JuanRuiz2406@users.noreply.github.com>
2021-04-04 22:13:49 -0600

    useCallback para solucionar warning

--- components/screens/reports.js
+++ components/screens/reports.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import { StyleSheet, View } from "react-native";
 import ActivityIndicator from "./activityIndicator";
 import { List } from "../report/index";
@@ -8,14 +8,13 @@ export default ({ navigation: { navigate } }) => {
   const [loading, setLoading] = useState(true);
   const [reports, setReports] = useState([]);
 
-  useEffect(async () => {
+  useEffect(() => {
     fetchReports();
   }, []);
 
-  const fetchReports = async () => {
+  const fetchReports = useCallback(async () => {
     await fetch("http://192.168.0.2:8080/report", {
       headers: {
-        method: "GET",
         Accept: "application/json",
         Authorization: "Bearer " + (await AsyncStorage.getItem("userToken")),
       },
@@ -26,7 +25,7 @@ export default ({ navigation: { navigate } }) => {
         setLoading(false);
         setReports(data);
       });
-  };
+  });
 
   const onPress = (report) => {
     navigate("Reporte Específico", {
