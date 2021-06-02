c07aac5a3a2403dd7e63070d6b0da8486e89d5d2
JuanRuiz2406 <44301111+JuanRuiz2406@users.noreply.github.com>
2021-06-02 00:42:24 -0600

    Mapa en vista reporte específico
    Se muestra en las coordenadas del reporte, y con un Marker que indica el punto exacto

--- components/report/showReport.js
+++ components/report/showReport.js
@@ -1,8 +1,17 @@
 import React from "react";
-import { Alert, StyleSheet, Image, Text, View } from "react-native";
+import {
+  Alert,
+  StyleSheet,
+  Image,
+  Text,
+  View,
+  Dimensions,
+  ScrollView,
+} from "react-native";
 import Button from "../ComponetsLogin/Button";
 import { useRoute } from "@react-navigation/native";
 import { deleteReport } from "../services/reports";
+import MapView, { Marker } from "react-native-maps";
 
 export default ({ report, details }) => {
   const route = useRoute();
@@ -20,46 +29,70 @@ export default ({ report, details }) => {
   };
 
   return (
-    <View style={styles.report}>
-      <Text style={styles.title}>{report.title}</Text>
-      <Text style={styles.titleLeft}>
-        <Text style={styles.bold}>Realizado por: </Text>
-        <Text>
-          {report.user.name} {report.user.lastname}
-        </Text>
-      </Text>
-      <Text style={styles.state}>
-        <Text style={styles.bold}>Estado: </Text>
-        <Text>{report.state}</Text>
-      </Text>
-      <Text style={[styles.titleLeft, styles.bold]}>Descripción: </Text>
-      <Text style={styles.description}>{report.description}</Text>
-      <Text style={[styles.titleLeft, styles.bold]}>Detalles:</Text>
-      {details.length > 0 ? (
-        details.map((x) => (
-          <Text style={styles.description} key={x.id}>
-            {x.updateDetail}
+    <ScrollView>
+      <View style={styles.report}>
+        <Text style={styles.title}>{report.title}</Text>
+        <Text style={styles.titleLeft}>
+          <Text style={styles.bold}>Realizado por: </Text>
+          <Text>
+            {report.user.name} {report.user.lastname}
           </Text>
-        ))
-      ) : (
-        <Text style={styles.description}>
-          No hay Detalles sobre este Reporte
         </Text>
-      )}
-      <Image
-        source={{ uri: report.imgURL}}
-        style={styles.image}
-      />
-      {canDelete && (
-        <Button mode="outlined" onPress={() => onDeleteReport(report.id)}>
-          Eliminar
-        </Button>
-      )}
-    </View>
+
+        <Text style={styles.state}>
+          <Text style={styles.bold}>Estado: </Text>
+          <Text>{report.state}</Text>
+        </Text>
+
+        <Text style={[styles.titleLeft, styles.bold]}>Descripción: </Text>
+        <Text style={styles.description}>{report.description}</Text>
+
+        <Text style={[styles.titleLeft, styles.bold]}>Ubicación:</Text>
+        <MapView
+          style={styles.map}
+          region={{
+            latitude: Number(report.coordenates.latitude),
+            longitude: Number(report.coordenates.longitude),
+            latitudeDelta: 0.003,
+            longitudeDelta: 0.003,
+          }}
+        >
+          <Marker
+            coordinate={{
+              latitude: Number(report.coordenates.latitude),
+              longitude: Number(report.coordenates.longitude),
+            }}
+          />
+        </MapView>
+
+        <Text style={[styles.titleLeft, styles.bold]}>Detalles:</Text>
+        {details.length > 0 ? (
+          details.map((x) => (
+            <Text style={styles.description} key={x.id}>
+              {x.updateDetail}
+            </Text>
+          ))
+        ) : (
+          <Text style={styles.description}>
+            No hay Detalles sobre este Reporte
+          </Text>
+        )}
+        <Image source={{ uri: report.imgURL }} style={styles.image} />
+        
+        {canDelete && (
+          <Button mode="outlined" onPress={() => onDeleteReport(report.id)}>
+            Eliminar
+          </Button>
+        )}
+      </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
+  map: {
+    height: Dimensions.get("window").width - 150,
+  },
   image: {
     width: 350,
     height: 250,
