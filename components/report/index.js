51fa1aa423ed55dee5fef59a4a491b282a8e497b
Marco Alvarado <marcotrabajos2013@gmail.com>
2021-06-18 04:00:08 -0600

    Revert "imagen estatica en list"
    This reverts commit 114379150f4a6a84db70f6cc62c2727388b95eb0.

--- components/report/index.js
+++ components/report/index.js
@@ -1,4 +1,3 @@
 export { default as List } from './list'
 export { default as ShowReport } from './showReport'
 export { default as CarouselImg } from './carouselImg'
-export { default as FirstPhoto } from './firstPhoto'
