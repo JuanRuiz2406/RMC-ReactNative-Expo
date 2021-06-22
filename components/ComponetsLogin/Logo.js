1001775948305467bae0b00cd04eaec856aa5451
dvperga <diegoskate98@gmail.com>
2021-06-22 17:34:24 -0600

    logos

--- components/ComponetsLogin/Logo.js
+++ components/ComponetsLogin/Logo.js
@@ -1,16 +1,20 @@
-import React from 'react'
-import { Image, StyleSheet } from 'react-native'
+import React from "react";
+import { Image, StyleSheet } from "react-native";
 
 export default function Logo() {
-    return <Image source={require('../../image/ReportsMyCity.png')} style={styles.image} />
+  return (
+    <Image
+      source={require("../../image/ReportsMyCity.png")}
+      style={styles.image}
+    />
+  );
 }
 
 const styles = StyleSheet.create({
-    image: {
-        width: 310,
-        height: 110,
-        marginBottom: 20,
-        marginVertical: 40,
-        marginLeft: '10%'
-    },
-})
+  image: {
+    width: 310,
+    height: 310,
+    marginBottom: 20,
+    marginVertical: 30,
+  },
+});
